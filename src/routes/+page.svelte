486064ce057855812b8dae4c5a38d9213a471e82
Sebastiano <sebastiano@racca.me>
2024-04-13 16:43:08 +0200

    Improve SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,35 @@
 <script lang="ts">
+  import SvelteSeo from "svelte-seo";
   import Subscribe from '$lib/components/SubscribeForm.svelte';
 </script>
+
+<SvelteSeo 
+  title="Subscribe to Sebastiano's Newsletter"
+  description="Subscribe to Sebastiano Racca's Newsletter to get Tech Tips Straight to your Inbox"
+  openGraph={{
+    title: "Subscribe to Sebastiano's Newsletter",
+    description: "Subscribe to Sebastiano Racca's Newsletter",
+    url: "https://newsletter.racca.me/",
+    type: "website",
+    site_name: "Sebastiano's Newsletter",
+  }}
+/>
+
+<svelte:head>
+  <script type="application/ld+json">
+    {
+      "@context": "http://schema.org",
+      "@type": "EmailNewsletter",
+      "name": "Sebastiano's Newsletter",
+      "description": "Tech Tips Straight to your Inbox!",
+      "publisher": {
+        "@type": "Person",
+        "name": "Sebastiano Racca",
+        "url": "https://newsletter.racca.me/"
+      }
+    }
+  </script>
+</svelte:head>
 
 <h1>Tech Tips Straight to your Inbox!</h1>
 <Subscribe />
